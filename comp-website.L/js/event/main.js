bc9ce5bcd18bda0104196baec661c451a30d130a
Lorna Doyle <ldoyle8@tcd.ie>
2019-04-01 19:18:35 +0100

    Event Title option after clicking add event button
    Event Title option after clicking add event button

--- comp-website.L/js/event/main.js
+++ comp-website.L/js/event/main.js
@@ -2,21 +2,46 @@ document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid' ],
+    plugins: [ 'interaction', 'dayGrid', 'list', 'googleCalendar' ],
     defaultView: 'dayGridMonth',
     header: {
-      center: 'addEventButton'
+      center: 'addEventButton',
+      left: 'prev,next today',
+      right: 'dayGridMonth,listYear'
     },
+    displayEventTime: false, // don't show the time column in list view
+
+    // THIS KEY WON'T WORK IN PRODUCTION!!!
+    // To make your own Google API key, follow the directions here:
+    // http://fullcalendar.io/docs/google_calendar/
+    googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
+
+    // Irish Holidays
+    events: 'en.irish#holiday@group.v.calendar.google.com',
+
+    eventClick: function(arg) {
+      // opens events in a popup window
+      window.open(arg.event.url, 'google-calendar-event', 'width=700,height=600');
+
+      arg.jsEvent.preventDefault() // don't navigate in main tab
+    },
+
+    loading: function(bool) {
+      document.getElementById('loading').style.display =
+        bool ? 'block' : 'none';
+    },
+    
     customButtons: {
       addEventButton: {
         text: 'add event...',
         click: function() {
           var dateStr = prompt('Enter a date in YYYY-MM-DD format');
           var date = new Date(dateStr + 'T00:00:00'); // will be in local time
-
+          var titleStr = prompt('Enter event title')
+          var title = '' + titleStr
           if (!isNaN(date.valueOf())) { // valid?
             calendar.addEvent({
-              title: 'dynamic event',
+              title: title,
               start: date,
               allDay: true
             });
